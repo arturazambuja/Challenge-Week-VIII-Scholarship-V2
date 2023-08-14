0c7873ddf176ed4ae38439e3a646669deaa3b8f7
Artur Azambuja <arturantoniolli@gmail.com>
2023-08-14 20:39:06 -0300

    Populating Script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,32 @@
+INSERT IGNORE INTO db_scholarship.coordinators  (first_name, last_name, email)
+VALUES
+    ('John', 'Smith', 'john.smith@example.com'),
+    ('Emily', 'Johnson', 'emily.johnson@example.com'),
+    ('Michael', 'Williams', 'michael.williams@example.com');
+INSERT IGNORE INTO db_scholarship.scrum_masters (first_name, last_name, email)
+VALUES
+    ('Sarah', 'Davis', 'sarah.davis@example.com'),
+    ('Kevin', 'Martinez', 'kevin.martinez@example.com'),
+    ('Jessica', 'Brown', 'jessica.brown@example.com');
+INSERT IGNORE INTO db_scholarship.instructors (first_name, last_name, email)
+VALUES
+    ('Matthew', 'Anderson', 'matthew.anderson@example.com'),
+    ('Laura', 'Taylor', 'laura.taylor@example.com'),
+    ('Robert', 'Miller', 'robert.miller@example.com');
+
+INSERT IGNORE INTO db_scholarship.students (first_name, last_name, email)
+VALUES
+    ('Christopher', 'Wilson', 'christopher.wilson@example.com'),
+    ('Amanda', 'Rodriguez', 'amanda.rodriguez@example.com'),
+    ('Benjamin', 'Martinez', 'benjamin.martinez@example.com'),
+    ('Olivia', 'Anderson', 'olivia.anderson@example.com'),
+    ('William', 'Hernandez', 'william.hernandez@example.com'),
+    ('Sophia', 'Thompson', 'sophia.thompson@example.com'),
+    ('Ethan', 'White', 'ethan.white@example.com'),
+    ('Isabella', 'Martinez', 'isabella.martinez@example.com'),
+    ('James', 'Lewis', 'james.lewis@example.com'),
+    ('Mia', 'Clark', 'mia.clark@example.com'),
+    ('Alexander', 'Walker', 'alexander.walker@example.com'),
+    ('Ava', 'Green', 'ava.green@example.com'),
+    ('Daniel', 'Hill', 'daniel.hill@example.com'),
+    ('Emma', 'Turner', 'emma.turner@example.com');
